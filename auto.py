8e00d51798e58afeceda8722499a661d0963391e
Hot Dominik <forro.dominik.adrian@diak.szbi-pg.hu>
2026-03-04 09:35:27 +0100

    á

--- auto.py
+++ auto.py
@@ -3,6 +3,19 @@ class Auto:
         self.marka = marka
         self.tipus = tipus
         self.evjarat = evjarat
-    
+        self.sebesseg = 0
+        
     def __str__(self):
-        return f"{self.marka},{self.tipus},{self.evjarat}"
+        return f"{self.marka},{self.tipus},{self.evjarat},{self.sebesseg}"
+    
+    def gyorsit(self,ertek):
+        self.sebesseg += ertek
+        if self.sebesseg > 200:
+            self.sebesseg = 200
+            print(f"NE GYORSíTSÁL MÁR MISTER VIN DIESEL! ({self.marka})")
+    
+    def fekez(self,ertek):
+        self.sebesseg -= ertek
+        if self.sebesseg < 0:
+            self.sebesseg = 0
+            print("HÚ DE NAGYOT FÉKEZTÉL ÖCSÉM!!!! ÓVATOSABBAN! KRESSZ MI AZ ANYÁMÉRT VAN?")
--- main.py
+++ main.py
@@ -1,5 +1,43 @@
+import random
 from auto import Auto
 
 auto1 = Auto("Toyota","Corolla",2015)
 auto2 = Auto("Ford", "Focus", 2015)
-print(auto1)
+auto3 = Auto("Suzuki","Swift",1998)
+auto4 = Auto("Banán Autó","Spongyabob",2020)
+auto4.gyorsit(9999999999999)
+avg = 0
+legidosebb = 0
+legidosebb_auto = None
+autok = [auto1,auto2,auto3]
+for auto in autok:
+    auto.gyorsit(random.randint(1,300))
+    avg += 2026-auto.evjarat
+    if legidosebb < auto.evjarat:
+        legidosebb_auto = auto
+        legidosebb = auto.evjarat
+avg = avg/len(autok)
+print(
+      f"""
+      Az autók átlagos évjárata: {round(avg,2)} év
+      A legidősebb autó: {legidosebb_auto}
+      """
+      )
+
+
+
+
+# autok = []
+
+# with open("autok_lista.txt", "r", encoding="UTF-8") as forrasfajl:
+#     for index, sor in enumerate(forrasfajl, start=1):
+#         sor = sor.strip()
+#         marka, tipus, evjarat = sor.split(";")
+
+#         auto = Auto(
+#             marka=marka,
+#             tipus=tipus,
+#             evjarat=int(evjarat)
+#         )
+
+#         autok.append(auto)
